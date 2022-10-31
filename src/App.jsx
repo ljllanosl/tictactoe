0be1230cc8765e4811e6d540a9df73fa6533a5df
ljllanosl <ljllanosl91@gmail.com>
2022-10-30 19:53:16 -0500

    added styles, learning scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Board from './components/Board';
 
+import './styles/root.scss';
+
 export default () => (
-  <>
+  <div className='app'>
     <h1>Tic Tac Toe</h1>
     <Board />
-  </>
+  </div>
 );
